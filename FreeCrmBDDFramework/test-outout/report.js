6ea538b697817509a7023329f0b5d8839a29ef73
ab rahim <49904704+abrahimpatel@users.noreply.github.com>
2020-03-28 19:18:06 +0530

    added cucumber script in my repositery

---
+++ FreeCrmBDDFramework/test-outout/report.js
@@ -0,0 +1,424 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("D:/selenium-cucum/src/main/java/Features/contacts.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Free CRM Create Contacts",
+  "description": "",
+  "id": "free-crm-create-contacts",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "Free CRM Create a new contact scenario",
+  "description": "",
+  "id": "free-crm-create-contacts;free-crm-create-a-new-contact-scenario",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user move on login page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user enters \"\u003cusername\u003e\" and \"\u003cpassword\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "user moves to new contact page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 12,
+  "name": "user enters contact details \"\u003cfirstname\u003e\" and \"\u003clastname\u003e\" and \"\u003cposition\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 15,
+  "name": "",
+  "description": "",
+  "id": "free-crm-create-contacts;free-crm-create-a-new-contact-scenario;",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password",
+        "firstname",
+        "lastname",
+        "position"
+      ],
+      "line": 16,
+      "id": "free-crm-create-contacts;free-crm-create-a-new-contact-scenario;;1"
+    },
+    {
+      "cells": [
+        "naveenk",
+        "test@123",
+        "Tom",
+        "Peter",
+        "Manager"
+      ],
+      "line": 17,
+      "id": "free-crm-create-contacts;free-crm-create-a-new-contact-scenario;;2"
+    },
+    {
+      "cells": [
+        "naveenk",
+        "test@123",
+        "David",
+        "Dsouza",
+        "Director"
+      ],
+      "line": 18,
+      "id": "free-crm-create-contacts;free-crm-create-a-new-contact-scenario;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 4102000,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 563700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "Free CRM Create a new contact scenario",
+  "description": "",
+  "id": "free-crm-create-contacts;free-crm-create-a-new-contact-scenario;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user move on login page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user enters \"naveenk\" and \"test@123\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "user moves to new contact page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 12,
+  "name": "user enters contact details \"Tom\" and \"Peter\" and \"Manager\"",
+  "matchedColumns": [
+    2,
+    3,
+    4
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_already_on_login_page()"
+});
+formatter.result({
+  "duration": 9731847300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.title_of_login_page_is_free_CRM()"
+});
+formatter.result({
+  "duration": 419342000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_move_on_login_page()"
+});
+formatter.result({
+  "duration": 5810066900,
+  "error_message": "org.openqa.selenium.WebDriverException: unknown error: cannot determine loading status\nfrom unknown error: missing or invalid \u0027entry.level\u0027\n  (Session info: chrome\u003d80.0.3987.149)\n  (Driver info: chromedriver\u003d2.27.440174 (e97a722caafc2d3a8b807ee115bfb307f7d2cfd9),platform\u003dWindows NT 10.0.18362 x86_64) (WARNING: The server did not provide any stacktrace information)\nCommand duration or timeout: 0 milliseconds\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-6F3L3JL\u0027, ip: \u0027192.168.0.110\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptSslCerts: true, applicationCacheEnabled: false, browserConnectionEnabled: false, browserName: chrome, chrome: {chromedriverVersion: 2.27.440174 (e97a722caafc2d..., userDataDir: C:\\Users\\HPX\\AppData\\Local\\...}, cssSelectorsEnabled: true, databaseEnabled: false, handlesAlerts: true, hasTouchScreen: false, javascriptEnabled: true, locationContextEnabled: true, mobileEmulationEnabled: false, nativeEvents: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: XP, platformName: XP, rotatable: false, takesHeapSnapshot: true, takesScreenshot: true, unexpectedAlertBehaviour: , unhandledPromptBehavior: , version: 80.0.3987.149, webStorageEnabled: true}\nSession ID: 365017b6d094e4c3f32f7a04c30f3038\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat java.lang.reflect.Constructor.newInstance(Unknown Source)\r\n\tat org.openqa.selenium.remote.ErrorHandler.createThrowable(ErrorHandler.java:214)\r\n\tat org.openqa.selenium.remote.ErrorHandler.throwIfResponseFailed(ErrorHandler.java:166)\r\n\tat org.openqa.selenium.remote.http.JsonHttpResponseCodec.reconstructValue(JsonHttpResponseCodec.java:40)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:80)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:44)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:285)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.click(RemoteWebElement.java:84)\r\n\tat stepDefinitions.LoginStepDefinition.user_move_on_login_page(LoginStepDefinition.java:44)\r\n\tat ✽.Then user move on login page(D:/selenium-cucum/src/main/java/Features/contacts.feature:7)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "naveenk",
+      "offset": 13
+    },
+    {
+      "val": "test@123",
+      "offset": 27
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_username_and_password(String,String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_clicks_on_login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_hopme_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_moves_to_new_contact_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Tom",
+      "offset": 29
+    },
+    {
+      "val": "Peter",
+      "offset": 39
+    },
+    {
+      "val": "Manager",
+      "offset": 51
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_contacts_details(String,String,String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.close_the_browser()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 80200,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 130900,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 149400,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 117500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "Free CRM Create a new contact scenario",
+  "description": "",
+  "id": "free-crm-create-contacts;free-crm-create-a-new-contact-scenario;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user move on login page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user enters \"naveenk\" and \"test@123\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "user moves to new contact page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 12,
+  "name": "user enters contact details \"David\" and \"Dsouza\" and \"Director\"",
+  "matchedColumns": [
+    2,
+    3,
+    4
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_already_on_login_page()"
+});
+formatter.result({
+  "duration": 8754204300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.title_of_login_page_is_free_CRM()"
+});
+formatter.result({
+  "duration": 704515600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_move_on_login_page()"
+});
+formatter.result({
+  "duration": 6350891500,
+  "error_message": "org.openqa.selenium.WebDriverException: unknown error: cannot determine loading status\nfrom unknown error: missing or invalid \u0027entry.level\u0027\n  (Session info: chrome\u003d80.0.3987.149)\n  (Driver info: chromedriver\u003d2.27.440174 (e97a722caafc2d3a8b807ee115bfb307f7d2cfd9),platform\u003dWindows NT 10.0.18362 x86_64) (WARNING: The server did not provide any stacktrace information)\nCommand duration or timeout: 0 milliseconds\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-6F3L3JL\u0027, ip: \u0027192.168.0.110\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptSslCerts: true, applicationCacheEnabled: false, browserConnectionEnabled: false, browserName: chrome, chrome: {chromedriverVersion: 2.27.440174 (e97a722caafc2d..., userDataDir: C:\\Users\\HPX\\AppData\\Local\\...}, cssSelectorsEnabled: true, databaseEnabled: false, handlesAlerts: true, hasTouchScreen: false, javascriptEnabled: true, locationContextEnabled: true, mobileEmulationEnabled: false, nativeEvents: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: XP, platformName: XP, rotatable: false, takesHeapSnapshot: true, takesScreenshot: true, unexpectedAlertBehaviour: , unhandledPromptBehavior: , version: 80.0.3987.149, webStorageEnabled: true}\nSession ID: c4dbb73b64579638cfa60507e0ac6ceb\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat java.lang.reflect.Constructor.newInstance(Unknown Source)\r\n\tat org.openqa.selenium.remote.ErrorHandler.createThrowable(ErrorHandler.java:214)\r\n\tat org.openqa.selenium.remote.ErrorHandler.throwIfResponseFailed(ErrorHandler.java:166)\r\n\tat org.openqa.selenium.remote.http.JsonHttpResponseCodec.reconstructValue(JsonHttpResponseCodec.java:40)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:80)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:44)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:285)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.click(RemoteWebElement.java:84)\r\n\tat stepDefinitions.LoginStepDefinition.user_move_on_login_page(LoginStepDefinition.java:44)\r\n\tat ✽.Then user move on login page(D:/selenium-cucum/src/main/java/Features/contacts.feature:7)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "naveenk",
+      "offset": 13
+    },
+    {
+      "val": "test@123",
+      "offset": 27
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_username_and_password(String,String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_clicks_on_login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_hopme_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_moves_to_new_contact_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "David",
+      "offset": 29
+    },
+    {
+      "val": "Dsouza",
+      "offset": 41
+    },
+    {
+      "val": "Director",
+      "offset": 54
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_contacts_details(String,String,String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.close_the_browser()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 109100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 97000,
+  "status": "passed"
+});
+});
