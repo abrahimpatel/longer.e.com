9be8dd55d9c0e71acea453f146b16391e7437533
ab rahim <49904704+abrahimpatel@users.noreply.github.com>
2020-03-28 19:37:28 +0530

    added new file

--- FreeCrmBDDFramework/src/main/java/MyRunner/TestRunner.java
+++ FreeCrmBDDFramework/src/main/java/MyRunner/TestRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 	@RunWith(Cucumber.class)
 	@CucumberOptions(
 			features = "D:\\selenium-cucum\\src\\main\\java\\Features\\contacts.feature", //the path of the feature files
-			glue={"stepDefinitions"}, //the path of the step definition files
+			glue={"stepDefinitions"}, //the path of the step definition filesjnjnvn
 			format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
 			monochrome = true, //display the console output in a proper readable format
 			strict = false, //it will check if any step is not defined in step definition file
